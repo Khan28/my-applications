88d881d5d1bebc352f984ad425317e0a2c16ed09
Khan82 <kadir.han82@hotmail.com>
2018-04-09 20:21:37 +0200

    Game

---
+++ C_Sharp_Form_On_Numara_Oyunu/C_Sharp_Form_On_Numara_Oyunu/Form1.Designer.cs
@@ -0,0 +1,165 @@
+namespace C_Sharp_Form_On_Numara_Oyunu
+{
+    partial class OnNumaraForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OnNumaraForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(334, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yeni 10 Numara Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(28, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(124, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(28, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(158, 25);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(28, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(192, 25);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(28, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(226, 25);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(28, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(260, 25);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(28, 20);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(294, 25);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(28, 20);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(328, 25);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(28, 20);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(362, 25);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(28, 20);
+            this.textBox10.TabIndex = 10;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(56, 97);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(336, 121);
+            this.listBox1.TabIndex = 12;
+            // 
+            // OnNumaraForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 265);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OnNumaraForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+    }
+}
+
---
+++ C_Sharp_Form_On_Numara_Oyunu/C_Sharp_Form_On_Numara_Oyunu/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C_Sharp_Form_On_Numara_Oyunu
+{
+    public partial class OnNumaraForm : Form
+    {
+        int[] GelenSayilar = new int[10];
+
+        private System.Windows.Forms.Button button1;
+
+        // 10 tane seçilen sayıları yerleştirmek için textbox kutucukları oluşturma
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+
+
+        // 80 numarada seçilen rasgele 10 numarayı yazdırmak  için listbox
+
+        private System.Windows.Forms.ListBox listBox1;
+ 
+        // Onnumara oyunu constructor: 
+
+        public OnNumaraForm()
+        {
+            //form oluşturulurken komponentleri oluştur ve özelliklerini ayarla
+
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Rastgele sayılar oluşturmak için random nesnesini kullanacağım.
+            //Random nesnesinin bir örneğini oluşturuyorum
+
+            Random random = new Random();
+
+            // Rastgele 1 ile 80 arasýnda 10 tane rakam belirlemek için oluşturduğum
+            // random nesnesini Next metosunu kullanacağım. Next metoduna parametre olarak
+            // rasgele sayı oluşturmak istediğim aralığı veriyorum. Burada 1 ile 80 arasında
+            // sayılar oluşturacak. Next metodunun sınırları almadığığı için 80 değerini 
+            // 81 olarak olarak giriyorum
+
+            for (int i = 0; i < 10; i++)
+            {
+                //Rastgele bir sayı oluşturuyorum ve bunu GelenSayilar dizisine ekliyorum
+ 
+                GelenSayilar[i] = random.Next(1, 81);
+
+                //Yeni oluşan sayı daha önce oluşup oluşmadığını kontrol etmek için
+                //for döngüsü ile GelenSayilar dizisinin bütün elemanlarını kontrol ediyorum.
+                //Eğer aynı sayı var ise i değerini 1 azaltarak bu sayının yeniden hesaplanmasını
+                //sağlıyorum.
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (GelenSayilar[j] == GelenSayilar[i])
+                    {
+                        i--;
+                    }
+                }
+            }
+
+            //belirlenen 10 rakamı sıralamak için iç içe iki for döngüsü ile dizi elemanları
+            //birbir ile karşılaştırıyorum. En küçük sayıyı en başa yerleştirerek sıralamayı
+            //yapıyorum
+
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (GelenSayilar[j] < GelenSayilar[i])
+                    {
+                        int sayi = GelenSayilar[j];
+                        GelenSayilar[j] = GelenSayilar[i];
+                        GelenSayilar[i] = sayi;
+                    }
+                }
+            }
+
+            //Sıralama işlemini Array sınıfı kullanılarak da yapılabilir.
+
+            Array.Sort(GelenSayilar);
+
+
+       
+            //belirlenen 10 rakamı textboxlara yazdırıyorum.
+            textBox1.Text = GelenSayilar[0].ToString();
+            textBox2.Text = GelenSayilar[1].ToString();
+            textBox3.Text = GelenSayilar[2].ToString();
+            textBox4.Text = GelenSayilar[3].ToString();
+            textBox5.Text = GelenSayilar[4].ToString();
+            textBox6.Text = GelenSayilar[5].ToString();
+            textBox7.Text = GelenSayilar[6].ToString();
+            textBox8.Text = GelenSayilar[7].ToString();
+            textBox9.Text = GelenSayilar[8].ToString();
+            textBox10.Text = GelenSayilar[9].ToString();
+
+
+            //her butona başladığında yeni 10 lu listeyi listebox kontrolünün ilk sırarısına 
+            //yerleştirmek için listbox ın insert metodunu kullanıyorum
+
+            string liste = GelenSayilar[0].ToString();
+            
+            for (int i = 1; i < 10; i++)
+			{
+                liste += " - " + GelenSayilar[i].ToString();
+			}
+
+            listBox1.Items.Insert(0,liste);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //formun başlarken formun başlığını değiştirmek istiyorum . Başlık On numara oyunu olsun
+            //bunun için this anahtar sözcüğünü kullanarak Text değerini değiştriyorum.
+
+            this.Text = "ON NUMARA OYUNU";
+        }
+    }
+}
